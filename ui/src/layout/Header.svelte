679f62745e023c3f8baa95f38987c4fecfbf6443
Rasmus <rasmus.raasuke@gmail.com>
2026-05-05 21:46:39 +0300

    set navigation links to left + clean up html structure

--- ui/src/layout/Header.svelte
+++ ui/src/layout/Header.svelte
@@ -6,14 +6,12 @@
 </script>
 
 <header class="bg-stone-50 border-b px-2 sm:px-3 py-3 flex flex-wrap gap-3 justify-between items-center">
-  <a href="/">
-    <div class="flex gap-2.5 items-center">
-      <div class="flex gap-2 items-center">
-        <img src="/favicon.svg" class="size-10" title="Time Tracker Logo" alt="Logo">
-        <h1 class="font-bold text-2xl">{t.title}</h1>
-      </div>
-    </div>
-  </a>
-  <Navigation/>
-  <Avatar />
+  <div class="flex items-center gap-8">
+    <a href="/" class="flex gap-2 items-center">
+      <img src="/favicon.svg" class="size-10" title="Time Tracker Logo" alt="Logo">
+      <h1 class="font-bold text-2xl">{t.title}</h1>
+    </a>
+    <Navigation/>
+  </div>
+  <Avatar/>
 </header>
--- ui/src/layout/Navigation.svelte
+++ ui/src/layout/Navigation.svelte
@@ -2,11 +2,12 @@
   import {user} from 'src/stores/auth'
 </script>
 
-{#if $user}
-  <a href="/">Home</a>
-  <a href="/projects">Projects</a>
+<div class="text-lg flex items-center gap-6 max-md:hidden">
+  {#if $user}
+    <a href="/projects">Projects</a>
     {#if $user.authRole === 'ADMIN'}
       <a href="/customers">Customers</a>
       <a href="/users">Users</a>
     {/if}
-{/if}
+  {/if}
+</div>
