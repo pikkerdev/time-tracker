a687222c803521c7c5289d2e17dcbce72a4fb9d2
liisakanemagi <liisa.kanemagi@gmail.com>
2026-05-07 16:28:15 +0300

    #200799568 prevent changing initial admin's role

--- src/users/UserRoutes.kt
+++ src/users/UserRoutes.kt
@@ -7,21 +7,25 @@ import klite.annotations.GET
 import klite.annotations.POST
 import klite.annotations.PathParam
 import klite.annotations.AttrParam
+import klite.Config
 
 @Access(AuthRole.ADMIN)
 class UserRoutes(
-  val userRepository: UserRepository
-)  {
+  val userRepository: UserRepository,
+){
+
+  private val initialAdmins = Config["INITIAL_ADMINS"].split(",")
+
   @GET
   fun list() = userRepository.list()
 
   @POST("/:id")
   fun save(userToSave: User, @PathParam id: Id<User>, @AttrParam user: User): User {
+    val email = userToSave.email.toString()
     require(id == userToSave.id) { "Wrong id" }
-    if (user.id == id) {
-      throw BadRequestException("errors.cannotModifyYourRole")
-    }
-    userRepository.save(userToSave)
+    if (user.id == id) throw BadRequestException("errors.cannotModifyYourRole")
+    else if (email in initialAdmins) throw BadRequestException("errors.cannotModifyInitialAdminRole")
+    else userRepository.save(userToSave)
     return userToSave
   }
 }
--- test/users/UserRoutesTest.kt
+++ test/users/UserRoutesTest.kt
@@ -7,6 +7,8 @@ import db.BaseMocks
 import db.Id
 import db.TestData.user
 import klite.BadRequestException
+import klite.Config
+import klite.Email
 import org.junit.jupiter.api.Test
 
 class UserRoutesTest: BaseMocks() {
@@ -20,4 +22,10 @@ class UserRoutesTest: BaseMocks() {
     expect (routes.save(user, user.id, currentUser)).toEqual(user)
     expect { (routes.save(user, user.id, user)) }.toThrow<BadRequestException>()
   }
+
+  @Test fun `modifying initial admin forbidden`() {
+    val currentUser = user.copy(email = Email("rasmus@pikker.dev"))
+    expect { (routes.save(user, user.id, currentUser)) }.toThrow<BadRequestException>()
+  }
+
 }
