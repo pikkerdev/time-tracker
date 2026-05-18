ea89ca58d0909a64b301109de18a17fd12615a13
liisakanemagi <liisa.kanemagi@gmail.com>
2026-05-18 21:23:52 +0300

    remove unused import

--- ui/src/pages/entries/TimeEntryPage.svelte
+++ ui/src/pages/entries/TimeEntryPage.svelte
@@ -5,7 +5,7 @@
   } from 'src/api/types'
   import {onMount} from 'svelte'
   import api from 'src/api/api'
-  import {formatAmount, t} from 'i18n'
+  import {t} from 'i18n'
   import TimeEntryTable from 'src/pages/entries/TimeEntryTable.svelte'
 
   let timeEntries: TimeEntryView[]
