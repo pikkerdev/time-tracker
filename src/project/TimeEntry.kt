717e1bc7cc0de3c9bd37860b883282de255a5f11
Anton Keks <anton@azib.net>
2026-05-19 11:57:30 +0300

    hourlyRate should be mandatory

--- src/project/TimeEntry.kt
+++ src/project/TimeEntry.kt
@@ -11,10 +11,10 @@ data class TimeEntry(
   val projectId: Id<Project>,
   val userId: Id<User> = Id(),
   val date: LocalDate = LocalDate.now(),
-  val hours: Double,
+  val hours: Decimal,
+  val hourlyRate: Decimal,
   val storyId: Long? = null,
   val description: String? = null,
-  val hourlyRate: Decimal = Decimal.ZERO,
   override val id: Id<TimeEntry> = Id()
 ): Entity<TimeEntry>
 
