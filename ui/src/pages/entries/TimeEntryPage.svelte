2fa4066234f383d1f46ece9575afeaee0b977951
KRSssssss <krisrobin.metsoja@gmail.com>
2026-05-20 13:49:27 +0300

    Admin can filter its own entries from all entries

--- ui/src/pages/entries/TimeEntryPage.svelte
+++ ui/src/pages/entries/TimeEntryPage.svelte
@@ -7,14 +7,28 @@
   import api from 'src/api/api'
   import {t} from 'i18n'
   import TimeEntryTable from 'src/pages/entries/TimeEntryTable.svelte'
+  import {user} from 'src/stores/auth'
 
   let timeEntries: TimeEntryView[]
+  let myTimeEntries: boolean = false
+
+  async function loadEntries(myTimeEntries = false){
+    timeEntries = await api.get(`projects/timeentries?myTimeEntries=${myTimeEntries}`)
+  }
 
   onMount(async () => {
-    timeEntries = await api.get('projects/timeentries')
+    await loadEntries(myTimeEntries)
   })
+
+  $: loadEntries(myTimeEntries)
 </script>
 
 <MainPageLayout class="relative" title={t.timeEntries.title}>
+  <div slot="title" class="flex items-center gap-4">
+    {#if $user.isAdmin}
+      {t.timeEntries.showMyTimeEntries}
+      <input type="checkbox" onchange={() => myTimeEntries = !myTimeEntries}/>
+    {/if}
+  </div>
   <TimeEntryTable {timeEntries}/>
 </MainPageLayout>
