f985b81542015aec50534878b997b12d963d0327
Anton Keks <anton@azib.net>
2026-05-22 12:19:37 +0300

    let github actions deploy timetracker automatically

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
-set -e
 
-SERVER=root@time.pikker.dev
-
-./gradlew test jar --info
-(cd ui && npm run test:run && npm run check && npm run build)
-
-echo "Started building"
-DOCKER_DEFAULT_PLATFORM=linux/amd64 docker compose build
-echo "Finished building"
-
-echo "Moving image to server"
-docker save time-tracker-app | gzip | ssh $SERVER 'gunzip | docker load'
-
-echo "Copying composes to server"
-scp compose.yml compose.server.yml $SERVER:timetracker/
-ssh $SERVER << END
-  cd timetracker && \
-  docker compose -f compose.yml -f compose.server.yml up -d --wait && \
-  docker logs -f timetracker-app-1
-END
+mkdir -p logs
+docker compose logs -t app 2>&1 | gzip > logs/app-"$(date +%Y-%m-%d_%H:%M:%S)".log.gz
+docker compose -f compose.yml -f compose.server.yml --env-file .env --env-file .env.prod up -d --pull always --no-build
