8d2360ef6d560a34c02bd60cdd6d7696f65faa72
liisakanemagi <liisa.kanemagi@gmail.com>
2026-05-22 15:18:49 +0300

    Logged in user sees /entry route when clicking on timetracker logo

--- ui/src/layout/Header.svelte
+++ ui/src/layout/Header.svelte
@@ -4,11 +4,13 @@
   import {t} from 'i18n'
   import Navigation from 'src/layout/Navigation.svelte'
   import {Link} from 'src/router'
+  import {user} from 'src/stores/auth'
+
 </script>
 
 <header class="bg-stone-50 border-b border-gray-300 px-2 sm:px-3 py-3 flex flex-wrap gap-3 justify-between items-center">
   <div class="flex items-center gap-8">
-    <Link to="/" class="flex gap-2 items-center">
+    <Link to={$user ? '/entry' : '/'} class="flex gap-2 items-center">
       <img src="/favicon.svg" class="size-10" title="Time Tracker Logo" alt="Logo">
       <h1 class="font-bold text-2xl">{t.title}</h1>
     </Link>
