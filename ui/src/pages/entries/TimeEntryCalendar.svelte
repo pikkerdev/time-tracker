524f8ade8fa87a7799b95c1a60af5842fa56c0ab
Anton Keks <anton@azib.net>
2026-06-02 17:33:03 +0300

    use the current lang for formatting

--- ui/src/pages/entries/TimeEntryCalendar.svelte
+++ ui/src/pages/entries/TimeEntryCalendar.svelte
@@ -3,14 +3,14 @@
   import Button from 'src/components/Button.svelte'
   import {onMount} from 'svelte'
   import FormField from 'src/forms/FormField.svelte'
-  import {today} from 'i18n'
+  import {lang, today} from 'i18n'
 
   export let dates: LocalDate[]
   export let timeEntryHours: Record<LocalDate, number>
   export let date = new Date().toISOString().slice(0, 10) as LocalDate
 
   function formatShortDate(date: LocalDate) {
-    return new Date(date).toLocaleDateString(undefined, {day: 'numeric', month: 'short', weekday: 'short'})
+    return new Date(date).toLocaleDateString(lang, {day: 'numeric', month: 'short', weekday: 'short'})
   }
 
   let container: HTMLElement
@@ -26,7 +26,7 @@
     {@const percentage = timeEntryHours[d] * 100 / 8}
     <Button onclick={() => date = d}
             class="default px-1.5! py-1! flex-col {d == date ? 'ring-2 ring-primary-500' : ''}"
-            style="background: linear-gradient(to top, #3b82f6 0%, #3b82f6 {percentage}%, #ffffff {percentage}%, #ffffff 100%);">
+            style="background: linear-gradient(to top, #3b82f6 0%, #3b82f6 {percentage}%, white {percentage}%);">
       <div class="whitespace-pre-line text-xs!" class:text-muted={[0, 6].includes(new Date(d).getDay())}>{formatShortDate(d).replace(', ', '\n').replaceAll(' ', '\u00A0')}</div>
       <div class="text-block">{timeEntryHours[d] ?? 0}h</div>
     </Button>
