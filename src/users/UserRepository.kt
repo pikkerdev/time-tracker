54fd46ff97f88757219d19f16cfff665f9fc0a74
Anton Keks <anton@azib.net>
2026-06-10 00:02:59 +0300

    no more sequence since Klite 1.9.0

--- src/users/UserRepository.kt
+++ src/users/UserRepository.kt
@@ -5,5 +5,5 @@ import klite.jdbc.exec
 import javax.sql.DataSource
 
 class UserRepository(db: DataSource): CrudRepository<User>(db, "users") {
-  fun setAppUser(user: User) { db.exec("call set_app_user(?)", sequenceOf(user.id)) }
+  fun setAppUser(user: User) { db.exec("call set_app_user(?)", user.id) }
 }
