ef4a4d1c412aca470ff4315ca3fc67e6294188d6
liisakanemagi <liisa.kanemagi@gmail.com>
2026-06-09 17:30:05 +0300

    add Pikker data to config

--- src/app/Config.kt
+++ src/app/Config.kt
@@ -5,3 +5,6 @@ import klite.d
 
 val vatRate = Config.optional("VAT_RATE", "0.24").d
 
+val companyName = Config.optional("COMPANY_NAME", "Pikker Technology OÜ")
+val companyAddress = Config.optional("COMPANY_ADDRESS", "Laeva tn 2, Tallinn 10111")
+val companyPhone = Config.optional("COMPANY_PHONE", "+372 53064406")
