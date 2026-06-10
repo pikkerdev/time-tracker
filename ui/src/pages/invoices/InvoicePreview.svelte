2b2f5ec1f584ae803d1a74b35d467518af8e7018
liisakanemagi <liisa.kanemagi@gmail.com>
2026-06-10 13:52:51 +0300

    join InvoiceContent & InvoicePreview by keeping the last

--- ui/src/pages/invoices/InvoicePreview.svelte
+++ ui/src/pages/invoices/InvoicePreview.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
 
-import InvoiceContent from 'src/pages/invoices/InvoiceContent.svelte'
+  import {showToast} from 'src/stores/toasts'
+  import api from 'src/api/api'
+  import {t, today} from 'i18n'
+  import type {InvoiceCreateRequest} from 'src/api/types'
+  import Button from 'src/components/Button.svelte'
 
-export let selectedEntryIds: string[] = []
+  export let description: string = ''
+  export let comment: string = ''
+  export let selectedEntryIds: string[] = []
 
+  async function createInvoice() {
+    const invoiceCreateRequest: InvoiceCreateRequest = {date: today, timeEntryIds: selectedEntryIds, description, comment}
+    await api.post('invoices', invoiceCreateRequest)
+    showToast(t.general.saved)
+  }
 </script>
 
 <div class="mx-auto flex max-w-6xl justify-center px-4 py-6 print:max-w-none print:px-0 print:py-0">
-  <InvoiceContent {selectedEntryIds}/>
+  <Button class="primary" label={t.invoices.create} onclick={createInvoice}/>
 </div>
 
 <style>
