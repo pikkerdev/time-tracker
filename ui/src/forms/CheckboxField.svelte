ccf1825a89ea1a5932852b438f70e6da48876c77
Rasmus <rasmus.raasuke@gmail.com>
2026-06-11 15:41:13 +0300

    #200906393 added CheckboxField

---
+++ ui/src/forms/CheckboxField.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let label: string|undefined = undefined
+  export let checked = false
+  export let helpText = ''
+
+  export let id = label ? label.replace(/\./g, '-') : ''
+</script>
+
+<div class="relative flex gap-2 items-center {$$props.class ?? ''}">
+  <input {id} type="checkbox" bind:checked {...$$restProps} class="{$$restProps.disabled && 'opacity-50'}">
+  <div>
+    {#if label}
+      <label class="select-none" for={id}>{label}</label>
+    {/if}
+    {#if helpText}<span class="help-text ml-1 cursor-default" title={helpText}>ⓘ</span>{/if}
+    <slot/>
+  </div>
+</div>
+
+<style>
+  @reference 'src/global.css';
+
+  :global(input[type=checkbox]) {
+    @apply focus:ring-primary-500 h-4 w-4 text-primary-500 border-gray-300 rounded;
+  }
+</style>
---
+++ ui/src/forms/CheckboxField.test.ts
@@ -0,0 +1,12 @@
+import {render} from '@testing-library/svelte'
+import CheckboxField from './CheckboxField.svelte'
+
+it('generates id', () => {
+  const {container} = render(CheckboxField, {label: 'merchant.consent'})
+  const input = container.querySelector('input') as HTMLInputElement
+  expect(input.id).to.eq('merchant-consent')
+  expect(input.type).to.eq('checkbox')
+  expect(input.checked).to.be.false
+  expect(input.required).to.be.false
+  expect(container.querySelector('label')!.textContent).to.eq('merchant.consent')
+})
