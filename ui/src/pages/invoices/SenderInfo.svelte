a301475f834e9ab6d302ea68266b888846c4b643
Rasmus <rasmus.raasuke@gmail.com>
2026-06-26 14:55:25 +0300

    delete unused components

--- ui/src/pages/invoices/SenderInfo.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import {t} from 'i18n'
-</script>
-
-<section class="sender-info mb-8">
-  <h2 class="text-lg font-bold">{t.company.name}</h2>
-  <p>{t.company.address}</p>
-  <p>{t.company.phone}</p>
-</section>
