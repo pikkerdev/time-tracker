c72ca55377f04c57e3d0e1e866b1ec1843b39b44
Rasmus <rasmus.raasuke@gmail.com>
2026-06-29 16:42:59 +0300

    introduce invoice lang changing

--- ui/i18n/index.ts
+++ ui/i18n/index.ts
@@ -7,20 +7,31 @@ export function changeLang(lang: typeof langs[number]) {
   location.reload()
 }
 
+export function changeLangInUrl(lang: typeof langs[number]) {
+  const url = new URL(location.href)
+  url.searchParams.set('lang', lang)
+  location.href = url.toString()
+}
+
 export function resolve(key: string, from: Record<string, any> = t): any {
   return key.split('.').reduce((acc, key) => acc && acc[key], from)
 }
 
 function choosePreferredLang() {
+  const urlLang = new URLSearchParams(location.search).get('lang')
+  if (urlLang && langs.includes(urlLang)) return urlLang
   let lang = localStorage?.['lang'] ?? navigator.language.split('-')[0]
   return langs.includes(lang) ? lang : langs[0]
 }
 
 async function load() {
   if (lang === 'en') return (await import('./en.json')).default
+  if (lang === 'et') return (await import('./et.json')).default
   else throw new Error('Unsupported lang: ' + lang)
 }
 
+export {langs}
+export const langsOptions = Object.fromEntries(langs.map(l => [l, l.toUpperCase()]))
 export const lang = choosePreferredLang()
 export let t: typeof en = await load()
 
