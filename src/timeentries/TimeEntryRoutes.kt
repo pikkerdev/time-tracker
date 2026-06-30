9fcd49c5cb316a2ad14d0f2009a871c3d45defa8
KRSssssss <krisrobin.metsoja@gmail.com>
2026-06-30 11:01:47 +0300

    #200906396 developer cannot add more than 24 hours a day

--- src/timeentries/TimeEntryRoutes.kt
+++ src/timeentries/TimeEntryRoutes.kt
@@ -4,17 +4,20 @@ import auth.Access
 import db.Id
 import klite.Decimal
 import klite.annotations.*
+import klite.d
 import projects.Project
 import projects.ProjectMemberRepository
 import projects.ProjectRepository
 import users.AuthRole.*
 import users.User
+import users.UserRepository
 import java.time.LocalDate
 
 class TimeEntryRoutes(
   val projectRepository: ProjectRepository,
   val projectMemberRepository: ProjectMemberRepository,
-  val timeEntryRepository: TimeEntryRepository
+  val timeEntryRepository: TimeEntryRepository,
+  val userRepository: UserRepository
 ) {
   @GET() @Access(ADMIN, INTERNAL, EXTERNAL, CUSTOMER)
   fun timeEntries(@AttrParam user: User, @QueryParam myTimeEntries: Boolean? = false, @QueryParam projectId: Id<Project>? = null, @QueryParam from: LocalDate? = null, @QueryParam to: LocalDate? = null) =
@@ -27,6 +30,7 @@ class TimeEntryRoutes(
     val member = projectMemberRepository.find(timeEntry.projectId, user.id) ?: throw NoSuchElementException("member")
     val rate = project.hourlyRates[member.role] ?: throw NoSuchElementException("hourlyRates")
     val newTimeEntry = timeEntry.copy(userId = user.id, hourlyRate = rate, role = member.role)
+    require(((userTimes(user, timeEntry.date, timeEntry.date).get(timeEntry.date)?: 0.d) + timeEntry.hours) <= 24.d){"Too many hours"}
     timeEntryRepository.save(newTimeEntry)
     return newTimeEntry
   }
@@ -35,6 +39,9 @@ class TimeEntryRoutes(
   fun editTimeEntry(@PathParam id: Id<TimeEntry>, timeEntry: TimeEntry): TimeEntry {
     require(timeEntry.invoiceId == null) {"Can not edit time entry that is in invoice"}
     require(id == timeEntry.id) { "Wrong id" }
+    val oldTimeEntry = timeEntryRepository.get(timeEntry.id)
+    val user = userRepository.get(timeEntry.userId)
+    require(((userTimes(user, timeEntry.date, timeEntry.date).get(timeEntry.date)?: 0.d) + timeEntry.hours - oldTimeEntry.hours) <= 24.d) {"Too many hours"}
     timeEntryRepository.save(timeEntry)
     return timeEntry
   }
