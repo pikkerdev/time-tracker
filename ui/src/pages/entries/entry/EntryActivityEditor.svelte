152b7af268591ed91e2a95741d39a5c5713252d7
KRSssssss <krisrobin.metsoja@gmail.com>
2026-07-09 12:46:41 +0300

    #200916245

--- ui/src/pages/entries/entry/EntryActivityEditor.svelte
+++ ui/src/pages/entries/entry/EntryActivityEditor.svelte
@@ -3,13 +3,14 @@
   export let label: string|undefined = undefined
   export let activity: string|undefined
   export let projectActivities: string[]
+  export let required = true
 
   let focused = false
 
 </script>
-<FormField {label} required={false} {...$$restProps} >
-  <input bind:value={activity} on:focus={() => focused = true} on:blur={() => focused = false}
-         required={false} list="activities">
+<FormField {label} {required} {...$$restProps} >
+  <input bind:value={activity} onfocus={() => focused = true} onblur={() => focused = false}
+         {required} list="activities">
   {#if focused}
     <datalist id="activities">
       {#each projectActivities as t}
