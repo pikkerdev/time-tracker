03915f0bcc3099f9f3fec77cef68b26a27787e90
KRSssssss <krisrobin.metsoja@gmail.com>
2026-08-06 16:30:23 +0300

    fix project description placement

--- ui/src/pages/projects/project/ProjectPage.svelte
+++ ui/src/pages/projects/project/ProjectPage.svelte
@@ -61,62 +61,64 @@
       <Button type="button" icon="trash" title={t.members.deleteMember} onclick={() => deleteProject(id)}/>
     {/if}
   </div>
-  <div class="flex flex-wrap gap-2 mt-4">
-    <div class="grid grid-cols-1 gap-x-16 gap-y-4">
-      <p class="text-lg">{ctx?.project?.description}</p>
-      <div class="flex flex-col">
-        <span class="text-lg font-semibold">{t.projects.overview}</span>
-        <hr class="text-pikker-gold border-t-3">
-        <div class="grid grid-cols-2 gap-x-4">
-          <span>{t.projects.totalHours}</span>
-          <span>{ctx?.stats?.totalHours}</span>
-          <span>{t.projects.unbilledHours}</span>
-          <span>{ctx?.stats?.unbilledHours}</span>
-          {#if ctx?.project?.currency}
-            <span>{t.projects.totalRevenue}</span>
-            <span>{ctx?.stats?.totalRevenue} {formatCurrency(ctx?.project?.currency)}</span>
-            <span>{t.projects.unbilledRevenue}</span>
-            <span>{ctx?.stats?.unbilledRevenue} {formatCurrency(ctx?.project?.currency)}</span>
-          {:else }
-            <span>{t.projects.totalRevenue}</span>
-            <span>{ctx?.stats?.totalRevenue}</span>
-            <span>{t.projects.unbilledRevenue}</span>
-            <span>{ctx?.stats?.unbilledRevenue}</span>
-          {/if}
-            </div>
-      </div>
-      <div class="flex flex-col">
-        <span class="text-lg font-semibold">{t.projects.hourlyRates}</span>
-        <hr class="text-pikker-gold border-t-3">
-        <div class="grid grid-cols-2 gap-x-4">
-          {#each Object.values(ProjectMemberRole) as role}
-            {@const rate = ctx?.project?.hourlyRates[role]}
-            {#if rate}
-              <span class="justify-start">{t.members.roles[role]}</span>
-              <span>{formatAmount(rate, ctx?.project?.currency)}</span>
-            {/if}
-          {/each}
-        </div>
-      </div>
-      {#if ctx?.members}
+  <div class="grid grid-cols-1">
+    <p class="text-lg">{ctx?.project?.description}</p>
+    <div class="flex flex-wrap gap-2 mt-4">
+      <div class="grid grid-cols-1 gap-x-16 gap-y-4">
         <div class="flex flex-col">
-          <span class="text-lg font-semibold">{t.members.title}</span>
+          <span class="text-lg font-semibold">{t.projects.overview}</span>
           <hr class="text-pikker-gold border-t-3">
-          <div class="flex flex-col">
-            {#each Object.values(ctx.members) as member}
-              <span>{member.user.name} - {member.member.role.toTitleCase()}</span>
+          <div class="grid grid-cols-2 gap-x-4">
+            <span>{t.projects.totalHours}</span>
+            <span>{ctx?.stats?.totalHours}</span>
+            <span>{t.projects.unbilledHours}</span>
+            <span>{ctx?.stats?.unbilledHours}</span>
+            {#if ctx?.project?.currency}
+              <span>{t.projects.totalRevenue}</span>
+              <span>{ctx?.stats?.totalRevenue} {formatCurrency(ctx?.project?.currency)}</span>
+              <span>{t.projects.unbilledRevenue}</span>
+              <span>{ctx?.stats?.unbilledRevenue} {formatCurrency(ctx?.project?.currency)}</span>
+            {:else }
+              <span>{t.projects.totalRevenue}</span>
+              <span>{ctx?.stats?.totalRevenue}</span>
+              <span>{t.projects.unbilledRevenue}</span>
+              <span>{ctx?.stats?.unbilledRevenue}</span>
+            {/if}
+              </div>
+        </div>
+        <div class="flex flex-col">
+          <span class="text-lg font-semibold">{t.projects.hourlyRates}</span>
+          <hr class="text-pikker-gold border-t-3">
+          <div class="grid grid-cols-2 gap-x-4">
+            {#each Object.values(ProjectMemberRole) as role}
+              {@const rate = ctx?.project?.hourlyRates[role]}
+              {#if rate}
+                <span class="justify-start">{t.members.roles[role]}</span>
+                <span>{formatAmount(rate, ctx?.project?.currency)}</span>
+              {/if}
             {/each}
           </div>
         </div>
-      {/if}
-      <div class="flex flex-col max-w-80">
-        <span class="text-lg font-semibold">{t.timeEntries.activities}</span>
-        <hr class="text-pikker-gold border-t-3">
-        <p>{ctx?.project?.activities?.map(a => a.split(',')).join(", ")}</p>
+        {#if ctx?.members}
+          <div class="flex flex-col">
+            <span class="text-lg font-semibold">{t.members.title}</span>
+            <hr class="text-pikker-gold border-t-3">
+            <div class="flex flex-col">
+              {#each Object.values(ctx.members) as member}
+                <span>{member.user.name} - {member.member.role.toTitleCase()}</span>
+              {/each}
+            </div>
+          </div>
+        {/if}
+        <div class="flex flex-col max-w-80">
+          <span class="text-lg font-semibold">{t.timeEntries.activities}</span>
+          <hr class="text-pikker-gold border-t-3">
+          <p>{ctx?.project?.activities?.map(a => a.split(',')).join(", ")}</p>
+        </div>
       </div>
-    </div>
-    <div class="grow" >
-      <ProjectBarChart label="Monthly hours" data={projectTimes}/>
+      <div class="grow" >
+        <ProjectBarChart label="Monthly hours" data={projectTimes}/>
+      </div>
     </div>
   </div>
 </MainPageLayout>
