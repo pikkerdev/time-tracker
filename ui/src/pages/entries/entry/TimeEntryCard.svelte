f815ddf8f1bc3f08d49b6225ae3c958aeef62ac2
Rasmus <rasmus.raasuke@gmail.com>
2026-08-07 16:03:17 +0300

    use TimeEntryCard instead of sortable table to show time entries on the time entry page

---
+++ ui/src/pages/entries/entry/TimeEntryCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import type {TimeEntryView} from 'src/api/types'
+  import Icon from 'src/icons/Icon.svelte'
+
+  export let entry: TimeEntryView
+</script>
+
+<div class="flex flex-col w-full bg-white px-4 py-2 rounded-lg border border-gray-300">
+  <span class="font-semibold">
+    {entry.entry.activity} - {entry.entry.hours}h
+  </span>
+  <p>{entry.entry.description}</p>
+  <div class="flex items-center justify-between gap-4 ">
+    <a href="/projects/{entry.entry.projectId}" class="flex items-center gap-1 text-gray-500 hover:text-pikker-gold transition-all duration-300">
+      <Icon class="size-4.5!" name="code-folder"/>
+      {entry.customerName} - {entry.projectName}
+    </a>
+    <div class="flex items-center gap-1">
+      <slot/>
+    </div>
+  </div>
+</div>
--- ui/src/pages/entries/entry/TimeEntryPage.svelte
+++ ui/src/pages/entries/entry/TimeEntryPage.svelte
@@ -1,39 +1,71 @@
 <script lang="ts">
   import MainPageLayout from 'src/layout/MainPageLayout.svelte'
-  import {today, toISODate} from 'i18n'
+  import {t, today, toISODate} from 'i18n'
   import TimeEntryForm from 'src/pages/entries/TimeEntryForm.svelte'
-  import type {LocalDate, TimeEntry, TimeEntryView} from 'src/api/types'
+  import type {Id, LocalDate, TimeEntry, TimeEntryView} from 'src/api/types'
   import api from 'src/api/api'
-  import TimeEntryTable from 'src/pages/entries/TimeEntryTable.svelte'
+  import TimeEntryCard from 'src/pages/entries/entry/TimeEntryCard.svelte'
   import TimeEntryCalendar from 'src/pages/entries/TimeEntryCalendar.svelte'
+  import Modal from 'src/components/Modal.svelte'
+  import {showToast} from 'src/stores/toasts'
+  import Button from 'src/components/Button.svelte'
 
   let date = today
   let timeEntries: TimeEntryView[] = []
   let timeEntry: TimeEntry = {date} as TimeEntry
   let timeEntryHours: Record<LocalDate, Record<string, number>> = {}
 
+  let editingTimeEntry: TimeEntry | false = false
+
   let dates = Array.from({length: 28}, (_, i) =>
     toISODate(new Date(), d => d.setDate(d.getDate() - i))).toReversed()
 
-  $: timeEntry.date = date
-  $: loadEntries(date)
-
   async function loadEntries(date: string) {
+    editingTimeEntry = false
     timeEntries = await api.get(`timeentries?myTimeEntries=true&from=${date}`)
     timeEntryHours = await api.get(`timeentries/user?from=${dates[0]}`)
   }
+
+  function handleEdit(entry: TimeEntryView) {
+    editingTimeEntry = entry.entry
+  }
+
+  async function handleDelete(id: Id<TimeEntry>) {
+    if (confirm(t.general.deleteConfirm)) {
+      await api.delete(`timeentries/${id}`)
+      showToast(`${t.general.deleted}`)
+      timeEntries = timeEntries.filter(i => i.entry.id !== id)
+      timeEntryHours
+    }
+  }
+
+  $: timeEntry.date = date
+  $: loadEntries(date)
 </script>
 
 <MainPageLayout class="flex flex-col gap-4 lg:gap-8">
   <div class="flex flex-col gap-4 items-center max-w-full">
     <TimeEntryCalendar bind:date {dates} {timeEntryHours}/>
-    <div class="flex flex-wrap-reverse justify-center gap-3 max-w-full">
-      <div class="px-4 overflow-x-scroll max-w-full">
-        <TimeEntryTable {timeEntries} narrow onSaved={() => loadEntries(date)}/>
+    <div class="w-full flex justify-center flex-wrap-reverse gap-4 lg:grid lg:grid-cols-5 lg:gap-22 px-6">
+      <div class="flex flex-col gap-2 lg:col-span-3 pb-3">
+        {#each timeEntries as entry (entry.entry.id)}
+          <TimeEntryCard {entry}>
+            <Button title={t.general.edit} icon="pencil" onclick={() => handleEdit(entry)}/>
+            <Button title={t.general.delete} iconClass="text-red-500" icon="trash" onclick={() => handleDelete(entry.entry.id)}/>
+          </TimeEntryCard>
+        {:else}
+          <div class="text-center py-4 text-muted">{t.general.noItems}</div>
+        {/each}
       </div>
-      <div class="w-70">
+      <div class="max-lg:w-130 lg:col-span-2">
         <TimeEntryForm bind:timeEntry onSaved={() => loadEntries(date)}/>
       </div>
     </div>
   </div>
 </MainPageLayout>
+
+<Modal bind:show={editingTimeEntry} title={t.timeEntries.title}>
+  {#if editingTimeEntry}
+    <TimeEntryForm bind:timeEntry={editingTimeEntry} onSaved={() => loadEntries(date)}/>
+  {/if}
+</Modal>
